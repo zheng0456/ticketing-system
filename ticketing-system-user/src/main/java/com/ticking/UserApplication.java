f4eedb219f72246880f1c019a12c90a71e9dafff
zheng0456 <2744437921@qq.com>
2025-11-06 15:44:40 +0800

    郑锦春
    上传项目

---
+++ ticketing-system-user/src/main/java/com/ticking/UserApplication.java
@@ -0,0 +1,12 @@
+package com.ticking;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class UserApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
